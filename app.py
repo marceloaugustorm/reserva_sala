5ccaae8af987c0ccc557635a843ddc7f980b7017
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-05-27 21:21:19 -0300

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from database import db
-from reserva_route import routes
+from Controllers.reserva_route import reserva_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-app.register_blueprint(routes)
+app.register_blueprint(reserva_bp)
 
 with app.app_context():
     db.create_all()
